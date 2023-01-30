899b181b67b50636071312c58eff4c534943b0ca
Sergey Reshetov <reshetov.sn@gmail.com>
2023-01-30 14:35:02 +0300

    create code "Hello World!"

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,2 +1,7 @@
-package hexlet.code;public class App {
+package hexlet.code;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
