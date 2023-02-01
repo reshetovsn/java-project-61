a21db9bbf90d5855f30d93596e51f013d04231ea
Sergey Reshetov <reshetov.sn@gmail.com>
2023-02-01 13:45:03 +0300

    add gamesList to class main.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
+        int choiceNumber = sc.nextInt();
+        System.out.println(choiceNumber);
+        if (choiceNumber == 0) {
+            sc.close();
+        } else if (choiceNumber == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.greeting();
+        } else if (choiceNumber == 2) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.greeting();
+            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+        }
     }
 }
