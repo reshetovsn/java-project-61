bbae1a5e8dbe5820ae4289305094f72b22497507
Sergey Reshetov <reshetov.sn@gmail.com>
2023-02-08 14:41:20 +0300

    fix 1st line in Greeting message in App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -13,7 +13,6 @@ public class Engine {
     }
     // Greeting. Used in all games at the very beginning //
     public static void greeting() {
-        System.out.println("Welcome to the Brain Games!");
         sc = new Scanner(System.in);
         System.out.println("May I have your name? ");
         userName = sc.next();
