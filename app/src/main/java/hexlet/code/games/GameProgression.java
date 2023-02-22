cba51d52b82f3593fb1f198855a662e6f626d170
Sergey Reshetov <reshetov.sn@gmail.com>
2023-02-22 12:48:08 +0300

    Modified GameProgression

--- app/src/main/java/hexlet/code/games/GameProgression.java
+++ app/src/main/java/hexlet/code/games/GameProgression.java
@@ -1,25 +1,20 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 import java.util.Arrays;
 
 import java.util.Random;
 
 public class GameProgression {
-    private static int init;
-    private static int step;
     private static final int VALUE_OF_STEP = 20;
     private static final int PROGRESSION_SIZE = 10;
 
-    private static String intoStr(int result) {
-        return String.valueOf(result);
-    }
-
     private static int[] getArray() {
         Random rand = new Random();
-        init = rand.nextInt(Engine.getUpperBound());
-        step = rand.nextInt(1, VALUE_OF_STEP);
+        int init = Utils.getUpperBound();
+        int step = rand.nextInt(1, VALUE_OF_STEP);
         int[] progression = new int[PROGRESSION_SIZE];
         var r = init;
         for (var i = 0; i < PROGRESSION_SIZE; i++) {
@@ -29,20 +24,25 @@ public class GameProgression {
         return progression;
     }
 
+    private static int getResult(int[] progression) {
+        Random randomElement = new Random();
+        int e = randomElement.nextInt(0, PROGRESSION_SIZE);
+        return progression[e];
+    }
+
     public static void getNumFromProgression() {
         String[] questions = new String[Engine.getRounds()];
         String[] results = new String[Engine.getRounds()];
         for (var i = 0; i < Engine.getRounds(); i++) {
             var array = getArray();
-            Random randomElement = new Random();
-            int e = randomElement.nextInt(0, PROGRESSION_SIZE);
-            results[i] = intoStr(array[e]); // choose a random element in array to swap it after
-            array[e] = -1; // change to unreal element in array
+            int result = getResult(array);
+            results[i] = String.valueOf(result); // choose a random element in array to swap it after
+//            result = -1; // change to unreal element in array
             // convert array into string without "[" & "]"
             String stringArray = Arrays.toString(array).replaceAll("\\[|]", "");
-            var words = stringArray.split(", ");
+            String[] words = stringArray.split(", ");
             for (var ii = 0; ii < words.length; ii++) { // swap "-1" for ".."
-                words[ii] = words[ii].equals("-1") ? ".." : words[ii];
+                words[ii] = words[ii].equals(String.valueOf(result)) ? ".." : words[ii];
             }
             questions[i] = "Question: " + String.join(" ", words);
         }
