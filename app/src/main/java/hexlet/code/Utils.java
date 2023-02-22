4dfc9984235c905e8d26c7e66e3fbfdaaa5682cd
Sergey Reshetov <reshetov.sn@gmail.com>
2023-02-22 12:08:10 +0300

    Modified class Utils

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,15 +3,19 @@ package hexlet.code;
 import java.util.Random;
 
 public class Utils {
-
-    static final int LOWER_BOUND = 1;
+    private static final int UPPER_BOUNDS_FOR_RAND = 100;
+    private static final int LOWER_BOUND = 1;
     static Random rand = new Random();
 
+    public static int getUpperBound() {
+        return UPPER_BOUNDS_FOR_RAND;
+    }
+
     public static int getRandomNumber() {
-        return rand.nextInt(Engine.getUpperBound());
+        return rand.nextInt(getUpperBound());
     }
 
     public static int getRandomNumWithBounds() {
-        return rand.nextInt(LOWER_BOUND, Engine.getUpperBound());
+        return rand.nextInt(LOWER_BOUND, getUpperBound());
     }
 }
