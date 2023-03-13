c0ad00f5ba428970db23ad3fa9e1c67a3e98b5c0
Sergey Reshetov <reshetov.sn@gmail.com>
2023-03-13 13:34:48 +0300

    fixed Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,19 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Engine {
-    private static Scanner sc;
-    private static String userName;
     public static final int ROUNDS = 3;
 
-    public static void greeting() {
-        sc = new Scanner(System.in);
-        System.out.println("May I have your name? ");
-        userName = sc.next();
-        System.out.println("Hello, " + userName + "!");
-    }
-
     public static void flow(String[][] questionsAndAnswers, String task) {
-        greeting();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name? ");
+        String userName = sc.next();
+        System.out.println("Hello, " + userName + "!");
         System.out.println(task);
         for (var i = 0; i < ROUNDS; i++) {
             String result = questionsAndAnswers[i][1];
